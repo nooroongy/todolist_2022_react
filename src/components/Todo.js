add91f8b5d94a944c3214f0374d245d7c5c07952
nooroongy <haerammy@gmail.com>
2022-04-25 19:48:49 +0900

    make todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import styledComponents from "styled-components";
 
-const Todo = ({ title, id }) => {
+const Todo = ({ title, id, tags = [] }) => {
     const [complete, setComplete] = useState(false);
 
 
 
-    return <div>
-        <div>{title}</div>
+    return <div className="todo__wrap">
+        <input className="todo__input" type='checkbox'></input>
+        <span className="todo__title">{title}</span>
+        <span className="todo__tags">
+            {tags.map(tag => <span>{tag}</span>)}
+        </span>
         <div>{id}</div>
     </div>
 }
